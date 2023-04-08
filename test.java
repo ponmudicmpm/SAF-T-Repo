90f2541cf8695424ebb5c595022033303d838a4d
Ponmudi C M <ponmudicmp@gmail.com>
2023-04-08 13:07:54 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
 public class test{
     public static void main(String[] args){
         int c = 42;
+=======
+        int c = 3;
+        int a = 20;
+>>>>>>> Stashed changes
         System.out.println("Hello bro");
         System.out.println("Hey I love github");
         System.out.println("Hey Github! I am Ponmudi");
