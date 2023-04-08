23948b884215e9979fc127b5634171894e5699ad
Ponmudi C M <ponmudicmp@gmail.com>
2023-04-08 12:27:22 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,5 +2,8 @@ public class test{
     public static void main(String[] args){
         System.out.println("Hello Ponmudi...  I am updating code in github");
         System.out.println("How it works?");
+        System.out.println("HI");
+        System.out.println("HI da Ponmudi");
+        System.out.println("Hi");
     }
 }
