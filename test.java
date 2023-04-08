c6691871d0c30e96f0a2ec80ae46695339fe1415
Ponmudi C M <105155650+ponmudicmpm@users.noreply.github.com>
2023-04-08 12:52:14 +0530

    Update my 1st code in github

--- test.java
+++ test.java
@@ -1,7 +1,5 @@
 public class test{
     public static void main(String[] args){
-        System.out.println("Hi da");
-        System.out.println("Who are you da branch");
-        System.out.println("I am the example of second branch");
+        System.out.println("Hello bro");
     }
 }
