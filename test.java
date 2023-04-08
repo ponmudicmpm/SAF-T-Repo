7b8cb5ca0451c04326844ccfc7c768dcab3d68f1
Ponmudi C M <105155650+ponmudicmpm@users.noreply.github.com>
2023-04-08 12:57:18 +0530

    Remove space

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ public class test{
     public static void main(String[] args){
         System.out.println("Hello bro");
         System.out.println("Hey I love github");
-        
         System.out.println("Hey Github! I am Ponmudi");
     }
 }
