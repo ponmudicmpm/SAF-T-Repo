64e5e41b321a6a34e53178c78fb2eddf5f8d6874
Ponmudi C M <105155650+ponmudicmpm@users.noreply.github.com>
2023-04-08 13:06:15 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test{
     public static void main(String[] args){
+        int c = 42;
         System.out.println("Hello bro");
         System.out.println("Hey I love github");
         System.out.println("Hey Github! I am Ponmudi");
